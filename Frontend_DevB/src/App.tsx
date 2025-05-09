1425b7b77b15f46714d3b2e9e256840f94c20276
Jayesh Akhare <147245391+jayeshakhare33@users.noreply.github.com>
2025-05-10 00:17:34 +0530

    Update App.tsx

--- Frontend_DevB/src/App.tsx
+++ Frontend_DevB/src/App.tsx
@@ -25,7 +25,6 @@ const App: React.FC = () => {
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/dashboard" element={<Dashboard />} />
-              //<Route path="*" element={<NotFound />} />
             </Routes>
           </TooltipProvider>
         </AuthProvider>
